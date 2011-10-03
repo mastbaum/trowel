f37ed479edbd3f060e451c9645f8f18619ad9d4e
Andy <amastbaum@gmail.com>
2011-10-03 14:08:37 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+trowel
+======
+trowel is a distributed Monte Carlo simulation generation tool, based on [dirt](http://github.com/mastbaum/dirt).
+
---
+++ settings.py
@@ -0,0 +1,23 @@
+# trowel Configuration
+
+# Project name
+project_name = 'trowel'
+
+## Database configuration
+# URL of couchdb server
+couchdb_host = 'http://localhost:5984'
+# couchdb database name
+couchdb_dbname = 'trowel'
+
+## Node configuration defaults
+# When adding a new node, enable it by default?
+node_enable_default = True
+
+## Logging configuration
+# Log file name
+log_file = 'trowel.log'
+# Email addresses to notify if a test fails
+notify_list = []
+# SMTP server is required for sending email notifications
+smtp_server = ''
+
